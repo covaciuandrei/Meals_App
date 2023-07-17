64734dea70b0c0080244d3b6174498be5c0d7494
covaciuandrei <covaciuandrei21@gmail.com>
2023-07-18 00:49:12 +0300

    animations added

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -2,15 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/models/meal.dart';
 
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
-  //initial val o lista empty
   FavoriteMealsNotifier() : super([]);
-  //in super pasam initial data
 
   bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
-    //nu poti modifica ceva in memorie cu .add sau .remove, trebuie creata
-    //alta chestie noua in memorie sa-i dam replace
-    //state holds the data
+
     if (mealIsFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
       return false;
@@ -18,8 +14,6 @@ class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
       state = [...state, meal];
       return true;
     }
-    //update the state in inmutable state
-    //without mutating the existing state in memory
   }
 }
 
@@ -27,3 +21,11 @@ final favoriteMealsProvider =
     StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
   return FavoriteMealsNotifier();
 });
+  //initial val o lista empty
+  //in super pasam initial data
+    //nu poti modifica ceva in memorie cu .add sau .remove, trebuie creata
+    //alta chestie noua in memorie sa-i dam replace
+    //state holds the data
+
+    //update the state in inmutable state
+    //without mutating the existing state in memory
